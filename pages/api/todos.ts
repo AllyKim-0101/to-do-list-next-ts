ab61c208a4b4f563e0da1855691c798063eda5ba
Ally <57968966+AllyKim-0101@users.noreply.github.com>
2022-07-10 11:56:56 +1000

    Add condition for post and other

--- pages/api/todos.ts
+++ pages/api/todos.ts
@@ -1,12 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import Cors from 'cors'
+import type { NextApiRequest, NextApiResponse } from "next";
+import Cors from "cors";
 
 // Initializing the cors middleware
 // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
 const cors = Cors({
-  methods: ['POST', 'GET', 'HEAD'],
-})
+  methods: ["POST", "GET", "HEAD", "DELETE"],
+});
 
 // Helper method to wait for a middleware to execute before continuing
 // And to throw an error when an error happens in a middleware
@@ -18,12 +18,12 @@ function runMiddleware(
   return new Promise((resolve, reject) => {
     fn(req, res, (result: any) => {
       if (result instanceof Error) {
-        return reject(result)
+        return reject(result);
       }
 
-      return resolve(result)
-    })
-  })
+      return resolve(result);
+    });
+  });
 }
 
 export default async function handler(
@@ -31,8 +31,13 @@ export default async function handler(
   res: NextApiResponse
 ) {
   // Run the middleware
-  await runMiddleware(req, res, cors)
-
+  await runMiddleware(req, res, cors);
+  console.log(req.body);
   // Rest of the API logic
-  res.json({ message: 'Hello Everyone!' })
+  // Add title
+  if (req.method === "POST") {
+    res.json(req.body);
+  } else {
+    res.json([]);
+  }
 }
