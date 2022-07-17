0c895443c3ab373db326434198d1e14de6438675
Ally <57968966+AllyKim-0101@users.noreply.github.com>
2022-07-17 13:47:31 +1000

    Seperate runMiddleware func

--- pages/api/todos.ts
+++ pages/api/todos.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import Cors from "cors";
+import { runMiddleware } from "../../utils/middleware";
 
 // Initializing the cors middleware
 // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
@@ -8,23 +9,6 @@ const cors = Cors({
   methods: ["POST", "GET", "HEAD", "DELETE"],
 });
 
-// Helper method to wait for a middleware to execute before continuing
-// And to throw an error when an error happens in a middleware
-function runMiddleware(
-  req: NextApiRequest,
-  res: NextApiResponse,
-  fn: Function
-) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result: any) => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
-
-      return resolve(result);
-    });
-  });
-}
 // anytime API gets request, it will execute the function
 export default async function handler(
   req: NextApiRequest,
